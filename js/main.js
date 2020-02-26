c94592bf2a8689650f4da5798265cc0f7d265f9a
Filippo Pittau <filippo.pittau@gmail.com>
2020-02-26 22:25:13 +0100

    Commit 4

--- js/main.js
+++ js/main.js
@@ -11,60 +11,88 @@ BONUS: all’inizio il software richiede anche una difficoltà all’utente che 
 Con difficoltà 0=> tra 1 e 100, con difficoltà 1 =>  tra 1 e 80, con difficoltà 2=> tra 1 e 50*/
 
 
-// 1 creare un ciclo con generatore di numeri tra 1 e 100 per 16 volte
-// 2 crearea un ciclo con un prompt per chiedere all'utente il numero
+/*
+1 genero 16 numeri random diversi da 1   a 100
+    1.1 creo un array vuoto
+    1.2 inserisco i numeri della bomba nell'array
+2 selezione numeroUtente
+    2.1 creo un array vuoto per i tentativi
+    2.2 chiediamo un numero tra 1 e 100
+3 Logica del gioco
+- ripetizione del numero
+    -il numero inserito è incluso nell'array dei numeri inseriti
+- Prendo una bomba
+    -il numero inserito è incluso nell'array delle bombe
+- Inserire il numero nell'array dei numeri inseriti
+- Se lunghezza numeri inseriti è uguale alla lunghezza massima hai vinto
 
+Gestine errorii
+1 numero >= di 100
+2 numero deve essere un numero
 
+------------------------------>PER GLI INSEGNANTI<--------------------------------
+HO RIVISTO TUTTO L'ESERCIZIO FATTO IN AULA E PASSATO PUNTO PER PUNTO PER RIVEDERE
+LA LOGICA VISTO GLI ENORMI PROBLEMI CHE HO AVUTO QUESTI GIORNI
+*/
 
+// var dimensioneCampo = 100; // scelta statica
+var dimensioneCampo = sceltaDifficolta(); // variabile dimensionecampo uttilizzata anche per scelta difficoltà
+var totaleMine = 16;//il totale delle mine è quello generato da genera random
+var bandierineMax = dimensioneCampo - totaleMine;//definisce il numero totale per posizionare le bandierine (dimensioneCampo - totaleMine 16)
 
-//ciclo per generare 16 volte un numero random
-var numeriArray = [];
-while (numeriArray.length < 16) { //ciclo while per generare in ogni caso 16 numeri senza doppioni
-    var numeroRandom = generaRandomMinMax(1, 100);//genera un numero da 1 a 100
-    if (numeriArray.includes(numeroRandom) == false ) {//se nel numerArray è incluso un numeroRandom è falsa
-    numeriArray.push(numeroRandom);//pusha ancora un numeroRandom in numeri Array
+var posizioneMine = minaIlCampo(dimensioneCampo, totaleMine);//richiama la funzione generaRandom per creare i numeri (mine )
+console.log(posizioneMine);// vedere i numeri delle (mine)
+var bandierinePiazzate = [];// array dove ci sono i numeri scelti dall'utente
+
+var boom = false;// variabile sentinella impostata su "false"
+while ((bandierinePiazzate.length < bandierineMax) && (boom === false)) {// ciclo while con && entrambi vere che restituisce "TRUE"
+    var bandierinaDaPiazzare = parseInt(prompt('Scrivi un numeri da 1 a ' + dimensioneCampo));// variabile con prompt per scegliere i numeri dimensione campo in base alla scelta della difficoltà dell'utente
+    if (!bandierinePiazzate.includes(bandierinaDaPiazzare)) {// 1° if condizione se bandierinaDaPiazzare (!non inclusa ) in  bandierinePiazzate continua il ciclo
+        if (!posizioneMine.includes(bandierinaDaPiazzare)) {//  2° if condizione se bandierinaDaPiazzare (! non inclusa)in posizioneMine continua            bandierinePiazzate.push(bandierinaDaPiazzare);
+            if (bandierinePiazzate.length == bandierineMax) {// 3° if condizione se i numeri scelti dall'utente (bandierine) sono uguali al numero bandierine max (dimensioneCampo- totaleMine) "VINTO"
+                alert('Vai a giocare al Superenalotto');// il maledetto alert che ti dice che "Hai vinto"
+            } else {
+                alert('Hai piazzato una bandierina');//alert che riprende il 3° if e annuncia sempre quando posizioni una bandierina
+            }
+        } else {// alert che riprende il 2° if stringa +il numero delle bandierine piazzate dall'utente
+            alert('BOOOM!! hai beccato una bomba! Hai piazzato ' + bandierinePiazzate.length + ' bandierine');
+            boom = true;// boom "true" hai beccato la bomba
+        }
+    } else {
+        alert('Hai già inserito questo numero');//alert per riprendere il 1° if
     }
 }
-var numero = [];
-console.log(numeriArray);
 
-
-// 2 crearea un ciclo con un prompt per chiedere all'utente il numero in questo coso è 5 per prova,
-//ma va cambiato a 84
-while (numero.length  < 5 && (controllo(numeriArray, numeroUtente)) != true) {
-    var numeroUtente = parseInt(prompt('inserisci un numero'));
-    if (controllo(numeriArray, numeroUtente) == true) {
-        alert('GAME OVER');
-
-    }else if (numero.includes(numeroUtente) == false) {
-        numero.push(numeroUtente);
-
-
-    }else if (numero.includes(numeroUtente) == true) {
-        alert('Hai già inserito questo numero');
-// il numero 5 poi v a cambiato a 84 come sopra
-    }else if (numero.length == 5 ) {
-        alert('Hai vinto');
-
+function sceltaDifficolta() {// funzione assegnata a variabile dimensioneCampo per scegliere difficoltà
+    var scelta = parseInt(prompt('Inserisci la difficoltà tra 1, 2 o 3'));
+    switch (scelta) {//switch uttilizzato per dare la possibilità all'utente di scegliere il grado di difficoltà
+        case 1:
+            var dimCampo = 100;// le varie dimensoni del campo
+            break;
+        case 2:
+            var dimCampo = 80;// le varie dimensoni del campo
+            break;
+        case 3:
+            var dimCampo = 50;// le varie dimensoni del campo
+            break;
+        default:
+            var dimCampo = 100;
     }
+    return dimCampo;
 }
 
-
-
-
-// funzione controllo numero vero o falso
-function controllo( listaRandom, num ){
-    var numeroTrovato = false;// variabile sentinella !!!
-    for (var i = 0; i < listaRandom.length; i++) {//ciclo di controllo
-        if (listaRandom[i] == num )  {// condizione se listaRandom uguale a num
-            numeroTrovato == true;
+function minaIlCampo(dimCampo, totMine) {//funzione e ciclo che richiama il generaRandom per generare i numeri (mine)
+    var posizMine = [];// array per posizionare i numeri (mine) genarati da generaRandom
+        while (posizMine.length < totMine) {// ciclo while con parametri per generare i mumeri (mmine)
+        var minaDaPiazzare = generaRandomMinMax(1, dimCampo);// variabile che richiama il generaRandom ( da 1 alla dimensione del campo)
+        if (!posizMine.includes(minaDaPiazzare)) {// condizione per  far si che i numeri generati non siano uguali attenzione (!)
+            posizMine.push(minaDaPiazzare);//e se sono generati uguali pusha "aggiunge" su posizMine  da (minaDaPiazzare=generaRandom)
         }
     }
-    return numeroTrovato;
+    return posizMine;// rritorna all'inizio del ciclo
 }
 
-//funzione per generare numero random
-function generaRandomMinMax(min, max) {
-    var numeroRandom = Math.floor(Math.random() * (max - min + 1) ) + min;
+function generaRandomMinMax(min, max) { // funzione che genera un numero random tra due valori dati in ingresso MIN e MAX, estremi inclusi
+    var numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
     return numeroRandom;
 }
